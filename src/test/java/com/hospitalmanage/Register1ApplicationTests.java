e8b587a3cdd2f7fb2607c9e8d21948c1b87db1b5
977076639@qq.com <15163@DESKTOP-F9ITPFM>
2019-04-12 16:47:58 +0800

    test again

---
+++ src/test/java/com/hospitalmanage/Register1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hospitalmanage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Register1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
